861ee273508f1160c6da0918aecbb48c0b198d55
Realyn04 <realyntabinas11@gmail.com>
2025-08-04 13:26:01 +0800

    Activity 1 and 2 : Understanding the Dart

---
+++ Act1.dart
@@ -0,0 +1,9 @@
+void main () {
+  var name = "Alice";
+  var greetings = "Welcome";
+    print ("Welcome, $name!");
+  
+  dynamic myVariable = 25;
+   myVariable = "Welcome, 25";
+  print (myVariable);
+}
